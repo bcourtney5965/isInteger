6529b02aa1ff85ac0a749d00eb7928d38c05a3e8
Brandon Courtney <brandon.h.courtney@gmail.com>
2015-06-05 13:03:42 -0500

    second commit

--- isInteger.js
+++ isInteger.js
@@ -1,9 +1,9 @@
 var x = +prompt("Please provide input");
-var checkNum = function(input) {
+function isInteger(input) {
 	if (typeof input === "number" && input % 1 ===0) {
 		console.log("Thats an integer");
 	} else {
 		console.log("That is not an integer");
 	}
 }
-checkNum(x);
+isInteger(x);
